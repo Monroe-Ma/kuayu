fe51d2abdccad85ccf8cf613a9968851377c0a58
Monroe <me_menglu@163.com>
2021-07-17 20:23:48 +0800

    frank.com/index.com 请求不到frank.js

---
+++ frank-com/public/frank.js
@@ -0,0 +1,9 @@
+const request = new XMLHttpRequest()
+request.open("get", "http:qq.com:8888/friends.json")
+request.onreadystatechange = () => {
+    console.log("/friends.json")
+    if (request.readyState === 4 && request.status === 200) {
+        console.log(request.response)
+     }
+}
+request.send()
